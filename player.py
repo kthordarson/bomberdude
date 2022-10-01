5e9dc8a026ed0ee26e6e73c02c3485c938cef01f
kthordarson <kthordarson@gmail.com>
2022-10-01 11:56:02 +0200

    playerzenfiskur

--- player.py
+++ player.py
@@ -1,26 +1,20 @@
-import time
 import pygame
-from pygame.sprite import Sprite
-import socket
-from pygame.math import Vector2
 from globals import BasicThing, Block, Bomb
 from loguru import logger
-from signal import SIGPIPE, SIG_DFL
-# from netutils import dataid, DataSender, DataReceiver, get_ip_address
-from globals import ResourceHandler, gen_randid
-from threading import Thread, Event
+from globals import gen_randid
+from threading import Thread
 from constants import *
-import pickle
-from network import send_data, receive_data, dataid
-# from multiprocessing import Queue
-from queue import Full
+from network import dataid
 from bclient import BombClient
 
 class Player(BasicThing, Thread):
 	def __init__(self, pos=None, visible=False, mainqueue=None):
 		Thread.__init__(self, daemon=True)
+		super().__init__(pos, None)
 		self.image = pygame.image.load('data/playerone.png')
-		BasicThing.__init__(self, pos, self.image)
+		self.size = PLAYERSIZE
+		self.image = pygame.transform.scale(self.image, self.size)
+		# BasicThing.__init__(self, pos, self.image)
 		self.mainqueue = mainqueue
 		self.visible = visible
 		self.client_id = gen_randid()
@@ -80,7 +74,7 @@ class Player(BasicThing, Thread):
 			self.client.send_pos((self.pos[0], self.pos[1]))
 			if not self.gotmap:
 				if self.client.gotmap:
-					self.mainqueue.put_nowait({'msgtype':'gamemapgrid', 'client_id':self.client_id, 'gamemapgrid':self.client.gamemapgrid})
+					#self.mainqueue.put_nowait({'msgtype':'gamemapgrid', 'client_id':self.client_id, 'gamemapgrid':self.client.gamemapgrid})
 					self.gotmap = True
 					logger.debug(f'[{self}] gotmap:{self.gotmap} grid:{len(self.client.gamemapgrid)}')
 
