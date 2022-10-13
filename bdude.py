aebf2726ee177a30c676008997060603ac0a177e
kthordarson <kthordarson@gmail.com>
2022-10-13 20:58:02 +0200

    try to fix getmap

--- bdude.py
+++ bdude.py
@@ -17,38 +17,6 @@ from player import Player
 from threading import Thread
 import threading
 
-class Dummyplayer():
-	def __init__(self):
-		self.vel = Vector2(0, 0)
-		self.pos = (0,0)
-		self.gridpos = [0,0]
-		#self.image = pygame.image.load('data/playerone.png')
-		self.size = (0,0)
-		self.image = None
-		self.rect = None
-		self.ready = False
-		self.client_id = None
-		self.name = f'player{self.client_id}'
-		self.netplayers = []
-		self.connected = False
-		self.kill = False
-		self.centerpos = None
-		self.speed = 3
-		self.gotmap = False
-		self.gotpos = False
-		self.serveraddress = ''
-		self.serverport = 9696
-		self.server = (self.serveraddress, self.serverport)
-		self.gamemap = []
-		self.bombs_left = 0
-		self.bombpower = 0
-		self.score = 0
-		self.hearts = 3
-		self.sendcnt = 0
-		self.recvcnt = 0
-		self.mapreqcnt = 0
-	def draw(self):
-		pass
 			
 class GameGUI:
 	def __init__(self, screen):
@@ -82,14 +50,13 @@ class Game(Thread):
 		self.bombs = Group()
 		self.flames = Group()
 		self.lostblocks = Group()
-		self.playerone = Dummyplayer()
 		# self.players.add(self.playerone)
 		self.authkey = 'foobar'
 		self.extradebug = False
 		self.screensize = (800, 600)
 
-	def __str__(self):
-		return f'[G] run:{self.running} p1 p1conn:{self.playerone.connected} p1clientconn:{self.playerone.connected} p1ready:{self.playerone.ready} p1gotmap:{self.playerone.gotmap} p1gotpos:{self.playerone.gotpos} np:{len(self.playerone.netplayers)} '
+#	def __str__(self):
+#		return f'[G] run:{self.running} p1 p1conn:{self.playerone.connected} p1ready:{self.playerone.ready} p1gotmap:{self.playerone.gotmap} p1gotpos:{self.playerone.gotpos} np:{len(self.playerone.netplayers)} '
 
 	def get_block_count(self):
 		# get number of killable blocks on map
@@ -111,7 +78,6 @@ class Game(Thread):
 		return {'bcnt': bcnt, 'pcnt': pcnt, 'wcnt': wcnt, 'ocnt': ocnt}
 
 	def run(self):
-		pygame.init()
 		pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP, pygame.USEREVENT])
 		pygame.display.set_mode((800,800), 0, 8)
 		self.screen = pygame.display.get_surface() #  pygame.display.set_mode(SCREENSIZE, 0, vsync=0)  # pygame.display.get_surface()#  pygame.display.set_mode(SCREENSIZE, 0, 32)
@@ -120,6 +86,7 @@ class Game(Thread):
 		self.gui = GameGUI(self.screen)
 		self.bgimage = pygame.transform.scale(pygame.image.load('data/blackfloor.png').convert(), (1000,900))
 		logger.debug(f'[ {self} ] started ')
+		self.playerone = Player()
 		while True:
 			if self.kill:
 				logger.warning(f'[ {self} ] game kill')
@@ -188,20 +155,28 @@ class Game(Thread):
 		elif msgtype == 'newnetpos':
 			posdata = gamemsg.get('posdata')
 			# blkcnt = len(self.blocks)
-			# if blkcnt <= 1:
-			# 	logger.warning(f'{self} block count mismatch! posdata={posdata} gamemsg={gamemsg}')
-			# 	return
 			client_id = posdata.get('client_id')
-			newpos = posdata.get('newpos')
-			newgridpos = posdata.get('newgridpos')
-
 			if client_id == self.playerone.client_id:
-				logger.info(f'newnetpos np={newpos} ngp={newgridpos} posdata={posdata} ')
-				self.playerone.pos = newpos
-				self.playerone.gridpos = newgridpos
-				self.playerone.gamemap.grid = posdata.get('griddata')
+				pass
+				#logger.info(f'newnetpos np={newpos} ngp={newgridpos} posdata={posdata} ')
 			else:
 				logger.warning(f'newnetpos clid mismatch clid={client_id} != {self.playerone.client_id} np={newpos} ngp={newgridpos} posdata={posdata} ')
+			self.playerone.pos = posdata.get('newpos')
+			self.playerone.gridpos = posdata.get('newgridpos')
+
+			#self.playerone.pos = Vector2(newpos[0], newpos[1])
+			self.playerone.rect.x = self.playerone.pos[0]
+			self.playerone.rect.y = self.playerone.pos[1]
+			self.playerone.gamemap.grid = posdata.get('griddata')
+			if len(self.blocks) != len(self.playerone.gamemap.grid[0])**2 or len(self.blocks) == 0:
+				logger.warning(f'{self} blkc={len(self.blocks)} p1g={len(self.playerone.gamemap.grid[0])**2} {len(self.playerone.gamemap.grid[0])} {len(self.playerone.gamemap.grid)} block count mismatch! ')
+				logger.warning(f'posdata={posdata} ')
+				logger.warning(f'gamemsg={gamemsg}')
+				logger.info(f'self.playerone.gamemap.grid={self.playerone.gamemap.grid}')
+				self.updategrid(self.playerone.gamemap.grid)
+				
+				#self.updategrid(gamemsg.get('gamemapgrid'))
+				#return
 
 		elif msgtype == 'flames':
 			flames = gamemsg.get('flamedata')
@@ -291,6 +266,9 @@ class Game(Thread):
 			logger.debug(f'gamemapgrid np={newpos} ngp={newgridpos}')
 
 	def updategrid(self, gamemapgrid):
+		if not gamemapgrid:
+			logger.warning(f'updategrid: no gamemapgrid')
+			return
 		old_blkcnt = len(self.blocks)
 		self.blocks.empty()
 		idx = 0
@@ -502,7 +480,7 @@ class Game(Thread):
 		# mainmenu
 		if selection == "Start":
 			self.gui.show_mainmenu ^= True
-			self.playerone = Player()
+			# self.playerone = Player()
 			self.players.add(self.playerone)
 			self.playerone.start()
 
@@ -556,6 +534,7 @@ class Game(Thread):
 				if event.key == pygame.K_f:
 					pass
 				if event.key == pygame.K_p:
+					print(self)
 					print('client netplayers:')
 					for np in self.playerone.netplayers:
 						print(f'\t{np} {self.playerone.netplayers[np]}')
@@ -640,6 +619,7 @@ class Game(Thread):
 if __name__ == "__main__":
 
 	#pygame.display.set_mode((800,600), 0, 8)
+	pygame.init()
 	game = Game()
 	game.daemon = True
 	game.run()
--- player.py
+++ player.py
@@ -74,6 +74,7 @@ class Player(BasicThing, Thread):
 			'score':self.score,
 			'bombpower':self.bombpower,
 			'events':[]}
+		self.st = Thread(target=self.send_update,daemon=True)
 
 
 	def __str__(self):
@@ -124,8 +125,8 @@ class Player(BasicThing, Thread):
 			'hearts':self.hearts,
 			'bombpower':self.bombpower,
 			}
-		#if self.ready:
-		self.eventqueue.put([payload])
+		if self.ready:
+			self.eventqueue.put([payload])
 
 	def send_bomb(self):
 		# send bomb to server
@@ -257,6 +258,9 @@ class Player(BasicThing, Thread):
 				return False
 			self.connected = True
 		logger.debug(f'[ {self} ] connect_to_server {self.server}')
+		self.send_request_clid()
+		self.send_maprequest(gridsize=15)
+		self.st.run()
 		return True
 
 	def movetogrid(self, movegrid):
@@ -395,13 +399,15 @@ class Player(BasicThing, Thread):
 				self.rect.x = self.pos[0]
 				self.rect.y = self.pos[1]
 				self.gridpos = newgridpos
-				if self.gotpos:
-					pass
-				if not self.gotmap:
-					logger.warning(f'{self} playerpos gotmap={self.gotmap} payload={len(payload)}')
-					pygame.event.post(Event(USEREVENT, payload={'msgtype':'gamemapgrid', 'client_id':self.client_id, 'gamemapgrid':self.gamemap.grid, 'pos':self.pos,'gotmap':self.gotmap,'gotpos':self.gotpos, 'newpos':self.pos, 'newgridpos':self.gridpos}))
-				logger.info(f'playerpos newpos={newpos} ngp={newgridpos} ogp={self.gridpos} payload={len(payload)}')
-				eventq.append(Event(USEREVENT, payload={'msgtype':'newnetpos', 'posdata':payload, 'pos':self.pos,'gotmap':self.gotmap,'gotpos':self.gotpos, 'newpos':newpos, 'newgridpos':self.gridpos}))
+				# if self.gotpos:
+				# 	pass
+				# if not self.gotmap:
+				# 	logger.warning(f'{self} playerpos gotmap={self.gotmap} payload={len(payload)}')
+				# 	eventq.append(Event(USEREVENT, payload={'msgtype':'gamemapgrid', 'client_id':self.client_id, 'gamemapgrid':self.gamemap.grid, 'pos':self.pos,'gotmap':self.gotmap,'gotpos':self.gotpos, 'newpos':self.pos, 'newgridpos':self.gridpos}))
+				# else:
+				# logger.info(f'playerpos newpos={newpos} ngp={newgridpos} ogp={self.gridpos} payload={len(payload)}')
+				eventq.append(Event(USEREVENT, payload={'msgtype':'newnetpos', 'posdata':payload, 'pos':self.pos,'gotmap':self.gotmap,'gotpos':self.gotpos, 'newpos':newpos, 'newgridpos':self.gridpos,'griddata':self.gamemap.grid}))
+					#eventq.append(Event(USEREVENT, payload={'msgtype':'gamemapgrid', 'client_id':self.client_id, 'gamemapgrid':self.gamemap.grid, 'pos':self.pos,'gotmap':self.gotmap,'gotpos':self.gotpos, 'newpos':self.pos, 'newgridpos':self.gridpos}))
 					#pygame.event.post(posmsg)
 			elif payload.get('msgtype') == 'mapfromserver':
 				# complete grid from server
@@ -426,11 +432,7 @@ class Player(BasicThing, Thread):
 
 
 	def run(self):
-		self.connect_to_server()
-		st = Thread(target=self.send_update(),daemon=True)
-		st.run()
-		self.send_request_clid()
-		self.send_maprequest(gridsize=15)
+		logger.debug(f'{self.connect_to_server()}')
 		payloads = []
 		#rcvr = Thread(target=get_netpayloads, args=(self.socket,), daemon=True)
 		#rcvr.start()
