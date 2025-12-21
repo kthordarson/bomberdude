8ff0b04778663ecd693a7438d5c6ac9a528e919d
kthordarson <kthordarson@gmail.com>
2025-12-21 16:13:35 +0000

    del load_image

--- utils.py
+++ utils.py
@@ -2,20 +2,6 @@ import struct
 import random
 import pygame
 
-def load_image(fullname, colorkey=None):
-	# fullname = os.path.join("data", name)
-	try:
-		image = pygame.image.load(fullname)
-		image = image.convert()
-		if colorkey is not None:
-			if colorkey == -1:
-				colorkey = image.get_at((0, 0))
-			image.set_colorkey(colorkey)
-		return image, image.get_rect()
-	except FileNotFoundError as e:
-		print(f"[load_image] {fullname} {e}")
-
-
 def gen_randid() -> str:
 	return generate_name()  # int(''.join([str(random.randint(0,9)) for k in range(10)]))
 
